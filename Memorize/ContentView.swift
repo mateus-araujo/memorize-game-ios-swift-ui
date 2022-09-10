d376793d95cb0ef7a983c8deeaded08ba3106e92
Mateus Araujo <rb.mateus.araujo@gmail.com>
2022-09-10 08:38:55 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Mateus Araújo on 10/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mateus Araújo on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
