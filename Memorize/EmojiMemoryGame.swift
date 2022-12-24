7a0b3e657bfb6e50f3fae251d0d348373b6f20f5
Mateus Araujo <rb.mateus.araujo@gmail.com>
2022-12-24 19:41:13 -0300

    create memory game model and view model

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,25 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Mateus Araújo on 24/12/22.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    static let emojis = ["😀","😃","😄","😁","😆","😅","😂","🤣","☺️","😊","😇","🙂","🙃","😉","😌","😍","😘","😗","😙","😚","😋","😜","😝","😛","🤑","🤗","🤓","😎"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+}
