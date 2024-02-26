107ab7dd5cb82f12cef969cce70c98a199185ed0
Mateus Araujo <rb.mateus.araujo@gmail.com>
2024-02-26 16:50:16 -0300

    show last score change with FlyingNumber component

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -74,16 +74,27 @@ struct EmojiMemoryGameView: View {
             return CardView(card)
                 .padding(4)
                 .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
+                .zIndex(scoreChange(causedBy: card) != 0 ? 100 : 0)
                 .onTapGesture {
-                    withAnimation {
-                        game.choose(card)
-                    }
+                    choose(card)
                 }
         }
     }
     
+    private func choose(_ card: Card) {
+        withAnimation {
+            let scoreBeforeChoosing = game.score
+            game.choose(card)
+            let scoreChange = game.score - scoreBeforeChoosing
+            lastScoreChange = (scoreChange, causedByCardId: card.id)
+        }
+    }
+    
+    @State private var lastScoreChange = (0, causedByCardId: "")
+    
     private func scoreChange(causedBy card: Card) -> Int {
-        return 0
+        let (amount, id) = lastScoreChange
+        return card.id == id ? amount : 0
     }
 }
 
